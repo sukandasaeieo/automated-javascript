fd218a437460cfd23156a3d0a22d2c91989fa21a
sukandasaeieo <sukandasaeieo@gmail.com>
2024-03-30 16:07:20 +0700

    forgetPassword test

---
+++ forgetPassword.js
@@ -0,0 +1,31 @@
+const { Builde, Builder, By, Key, until } = require("selenium-webdriver");
+
+async function loginTest() {
+  //launch the browser
+  let driver = await new Builder().forBrowser("chrome").build();
+
+  try {
+    // Navigate to the website
+    await driver.get("https://practice.expandtesting.com/forgot-password");
+
+    // Select input elements and fill them out
+    await driver.findElement(By.id("email")).sendKeys("test@gmail.com");
+
+
+    // Select Retrieve password button and click action
+    await driver.findElement(By.css("button[type='submit']")).click();
+
+    await driver.wait(until.elementLocated(By.id("confirmation-alert")), 10000);
+    
+    // make it delay for 5 seconds
+    await driver.sleep(5000);
+  
+  } catch (error) {
+    console.error("An error occurred:", error);
+  } finally {
+    // Close the browser
+    await driver.quit();
+  }
+}
+
+loginTest();
