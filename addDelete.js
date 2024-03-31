c3c5d3c63461ae1e2fab42560eb61bef4f933ce1
sukandasaeieo <sukandasaeieo@gmail.com>
2024-03-31 15:52:40 +0700

    addDelete - commit file

---
+++ addDelete.js
@@ -0,0 +1,26 @@
+const { Builder, By, Key, until } = require('selenium-webdriver');
+
+async function addDelete() {
+  let driver = await new Builder().forBrowser('chrome').build();
+  try {
+    await driver.get("https://practice.expandtesting.com/add-remove-elements");
+
+    // Add 5 items
+    for (let i = 0; i < 5; i++) {
+      await driver.findElement(By.className('btn-primary')).click();
+      await driver.sleep(1000); 
+    }
+
+    // Delete each item
+    let elements = await driver.findElements(By.className('added-manually'));
+    for (let i = 0; i < elements.length; i++) {
+      await elements[i].click();
+      await driver.sleep(1000); 
+    }
+  } catch (error) {
+    console.error('An error occurred:', error.message);
+  } finally {
+    await driver.quit();
+  }
+}
+addDelete();
